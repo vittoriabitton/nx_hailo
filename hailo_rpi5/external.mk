55f3347b7edbee5665583271bad941cfcc38733d
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2025-04-19 01:30:51 -0300

    iterate

--- hailo_rpi5/external.mk
+++ hailo_rpi5/external.mk
@@ -1,4 +1,8 @@
+# Include custom packages
 include $(sort $(wildcard $(NERVES_DEFCONFIG_DIR)/packages/*/*.mk))
 
 # Add hailort packages to the default build targets
 TARGET_PACKAGES += hailort hailort-drivers
+
+# Register the custom packages path
+BR2_EXTERNAL := $(NERVES_DEFCONFIG_DIR)
