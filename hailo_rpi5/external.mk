348bbc3748416db8e09863445ddf00739269c907
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2025-04-19 01:18:39 -0300

    iterate

--- hailo_rpi5/external.mk
+++ hailo_rpi5/external.mk
@@ -1,5 +1,4 @@
 include $(sort $(wildcard $(NERVES_DEFCONFIG_DIR)/packages/*/*.mk))
 
 # Add hailort packages to the default build targets
-LINUX_DEPENDENCIES += hailort hailort-drivers
 TARGET_PACKAGES += hailort hailort-drivers
