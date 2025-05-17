eaca2c2c74e166eee87d97a6dcc5bde27e51b76f
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2025-05-16 23:31:26 -0400

    chore: add wifi settings

--- nx_hailo/config/target.exs
+++ nx_hailo/config/target.exs
@@ -54,7 +54,20 @@ config :vintage_net,
        type: VintageNetEthernet,
        ipv4: %{method: :dhcp}
      }},
-    {"wlan0", %{type: VintageNetWiFi}}
+    {"wlan0",
+     %{
+       type: VintageNetWiFi,
+       vintage_net_wifi: %{
+         networks: [
+           %{
+             key_mgmt: :wpa_psk,
+             ssid: "NervesNetwork",
+             psk: "SecureWifiPassword"
+           }
+         ]
+       },
+       ipv4: %{method: :dhcp}
+     }}
   ]
 
 config :mdns_lite,
